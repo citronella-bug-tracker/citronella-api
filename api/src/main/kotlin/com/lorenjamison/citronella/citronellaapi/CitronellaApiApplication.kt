2edefa4f9c86d1e9f5f4648dc493492d6e390c66
Loren Jamison <loren.jamison@gmail.com>
2021-03-29 20:04:49 -0500

    New Spring initializr project.

---
+++ api/src/main/kotlin/com/lorenjamison/citronella/citronellaapi/CitronellaApiApplication.kt
@@ -0,0 +1,11 @@
+package com.lorenjamison.citronella.citronellaapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CitronellaApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CitronellaApiApplication>(*args)
+}
---
+++ api/src/test/kotlin/com/lorenjamison/citronella/citronellaapi/CitronellaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lorenjamison.citronella.citronellaapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CitronellaApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
