970379f7ddaa1a746da3547b1bae5c06239fada7
Loren Jamison <loren.jamison@gmail.com>
2022-02-16 12:45:25 -0600

    Add endpoint to get currently logged in user.

--- src/main/groovy/com/lorenjamison/citronella/api/api/v1/UserApi.groovy
+++ src/main/groovy/com/lorenjamison/citronella/api/api/v1/UserApi.groovy
@@ -5,7 +5,8 @@ import com.lorenjamison.citronella.api.model.CitronellaUser
 import com.lorenjamison.citronella.api.service.UserService
 import groovy.transform.CompileStatic
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -20,18 +21,25 @@ class UserApi {
         this.userService = userService
     }
 
+    @GetMapping(value = ['/'])
+    ResponseEntity getLoggedInUser() {
+        String email = SecurityContextHolder.context.authentication.name
+        ResponseEntity.ok(userService.getUserByEmailAddress(email))
+    }
+
     @PutMapping(value = ['/'], consumes = ['application/JSON'])
-    CitronellaUser upsertUser(@RequestBody CitronellaUser user) {
-        userService.upsertUser(user)
+    ResponseEntity upsertUser(@RequestBody CitronellaUser user) {
+        ResponseEntity.ok(userService.upsertUser(user))
     }
 
     @GetMapping(value = ['/{id}'])
-    CitronellaUser getUserById(@PathVariable Long id) {
-        userService.getUserById(id)
+    ResponseEntity getUserById(@PathVariable Long id) {
+        CitronellaUser user = userService.getUserById(id)
+        user ? ResponseEntity.ok(user) : ResponseEntity.notFound().build()
     }
 
     @PutMapping(value = ['/{id}/changepassword'], consumes = ['application/JSON'])
-    void changeUserPassword(@PathVariable Long id, @RequestBody ChangePasswordRequest request) {
-
+    ResponseEntity changeUserPassword(@PathVariable Long id, @RequestBody ChangePasswordRequest request) {
+        ResponseEntity.badRequest().build() //TODO: Not implemented yet
     }
 }
